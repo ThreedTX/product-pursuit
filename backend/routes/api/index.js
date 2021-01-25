1432f31eca5616b7beaf2ac1153d1a72193131d6
Reed <ReedWinstonNorton@gmail.com>
2021-01-25 15:21:41 -0600

    login route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,18 +1,15 @@
 const router = require('express').Router();
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
 
 /*The main purpose of my Express application
 is to be a REST API server. All the API routes will
 be served at URL's starting with /api/. */
 
 
-router.post('/test', function (req, res) {
-  res.json({ requestBody: req.body });
-});
-/*A router is created and an API test route is added
-to the router. The API test route is accepting requests
-with the URL path of /api/test with the HTTP verb of POST.
-It sends a JSON response of whatever is in the body
-of the request. */
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 
 
--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,4 +1,40 @@
-const express = require('express')
+const express = require('express');
+const asyncHandler = require('express-async-handler');
+/*The asyncHandler function from express-async-handler
+will wrap asynchronous route handlers and custom middlewares. */
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+/* ----- ----- */
+/* POST /api/session */
+/* ----- ----- ----- ----- ----- ----- ----- -----  */
+router.post(
+  '/',
+  asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    /*If there is no user returned from the login static method, it creates a
+    "Login failed" error and invokes the next error-handling middleware with it.*/
+    if (!user) {
+      const err = new Error('Login failed');
+      err.status = 401;
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
+      return next(err);
+    }
+
+    await setTokenCookie(res, user);
+    /* If there is a user returned from the method, then call setTokenCookie
+    and return a JSON response with the user information */
+    return res.json({
+      user,
+    });
+  }),
+);
 
 module.exports = router;
