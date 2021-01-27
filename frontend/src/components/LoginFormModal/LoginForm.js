32ffa8409162057b05cd21d0f37c0b3c6fe4f509
Reed <ReedWinstonNorton@gmail.com>
2021-01-27 01:23:24 -0600

    A decent amount of CSS on Log In Modal

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
 import "./LoginForm.css";
+import logo from './images/website_logo_transparent_background.png';
 
 function LoginForm() {
   const dispatch = useDispatch();
@@ -20,35 +21,57 @@ function LoginForm() {
   };
 
   return (
-    <>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
-    </>
+    <div className="divider__div">
+      <h1>Log In to Product Pursuit</h1>
+      <p>Need an account?<a> Sign Up</a></p>
+      <div className="left-container">
+        <form className="log-in__form" onSubmit={handleSubmit}>
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+          <div className="input-icons">
+            <div className="input-icons__container">
+              <i class="fas fa-user"></i>
+            </div>
+            <input
+              type="text"
+              placeholder=" "
+              value={credential}
+              onChange={(e) => setCredential(e.target.value)}
+              required
+            />
+            <span class="floating-placeholder">Username or Email</span>
+          </div>
+          <div className="input-icons">
+            <div className="input-icons__container">
+              <i class="fas fa-key"></i>
+            </div>
+            <input
+              type="password"
+              placeholder=" "
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+            <span class="floating-placeholder"> Password</span>
+          </div>
+          <button className="log-in__button" type="submit">Log In</button>
+        </form>
+      </div >
+      <div className="right-container">
+        <h2>Welcome back!</h2>
+        <div className="right-container-content__div">
+          <img className="logo" src={logo} />
+          <ul>
+            {errors.map((error, idx) => (
+              <li key={idx}>{error}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div >
   );
 }
 
