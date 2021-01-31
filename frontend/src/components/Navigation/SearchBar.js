aa90c7f34956123993caa139ea5ed8d17009e0e1
Reed <ReedWinstonNorton@gmail.com>
2021-01-30 23:53:11 -0600

    Basic Search input field

--- frontend/src/components/Navigation/SearchBar.js
+++ frontend/src/components/Navigation/SearchBar.js
@@ -1,5 +1,17 @@
+import { useState } from "react";
+
 function SearchBar() {
 
+  const [search, setSearch] = useState("");
+
+  return (
+    <input
+      type="search"
+      placeholder="Discover your next favorite thing..."
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+    />
+  )
 }
 
 export default SearchBar;
