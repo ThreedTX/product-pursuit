7ed4d28bb7276b3b9b575d0ca5f3f458d902c541
Reed <ReedWinstonNorton@gmail.com>
2021-01-31 23:22:17 -0600

    Product List css

--- frontend/src/components/ProductList/ProductList.js
+++ frontend/src/components/ProductList/ProductList.js
@@ -1,36 +1,43 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
-import { getProducts } from "../../store/products"; /** */
+import { getProducts } from "../../store/products";
+// import { getReviews } from "../../store/review";
+import logo_80x80 from './images/logo_80x80.png';
 import './ProductList.css';
 
 function ProductList() {
   const dispatch = useDispatch();
 
-  // const productId = Number.parseInt(useParams().productId);
-  // const product = useSelector(state => state.product[productId]);
-  // console.log(productId);
-
   const products = useSelector((state) => Object.values(state.product));
-  // const products2323 = useSelector(state => {
-  //   return state.pokemon.list.map(pokemonId => state.pokemon[pokemonId]);
-  // });;
 
+  let rng = Math.floor(Math.random() * 3) + 1;
 
   /** */
   useEffect(() => {
     dispatch(getProducts());
+    // dispatch(getReviews());
   }, [dispatch]);
 
 
   let key = 0;
 
   return (
-    <div>
-      <ul>
+    <div className="products-list__container">
+      <p className="products-list__title">Products</p>
+      <ul className="products-list__li-container">
         {products.map(product => (
-          <li key={key++}>
-            <NavLink key={product.id} id={product.id} to={`/products/${product.id}`}>{product.name}</NavLink>
+          <li key={key++} className="products-list">
+            <div className="products-list__content-container">
+              <img className="logo_80x80 float-left" src={logo_80x80} />
+              <div className="float-left products-list__content">
+                <NavLink key={product.id} id={product.id} to={`/products/${product.id}`}>{product.name}</NavLink>
+                <p className="products-list__description">{product.description}</p>
+                <div className="products-list__comment">
+                  <i class="fas fa-comment"><p className="prodcuts-list__comment--count">{rng}</p></i>
+                </div>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
